681ba6eb19384c41f9dbe9a301514e735b3556e4
HoangLong-88 <quoccuong130406@gmail.com>
2025-05-10 20:15:52 +0700

    init thong ke

---
+++ BWD/JavaScript/counter.js
@@ -0,0 +1,13 @@
+function animateCounter(id, targetNumber, duration) {
+    const element = document.getElementById(id);
+    let startNumber = 0;
+    const stepTime = Math.abs(Math.floor(duration / targetNumber));
+    
+    const timer = setInterval(() => {
+      startNumber++;
+      element.textContent = startNumber;
+      if (startNumber >= targetNumber) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+  }
